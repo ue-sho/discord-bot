78be8b3bdfb84ad6463bfe58f2471faa1f136a62
ue-sho <tssu45@gmail.com>
2024-10-25 08:48:47 +0900

    Add a slash command

--- src/bot/discord_event.py
+++ src/bot/discord_event.py
@@ -1,40 +1,42 @@
 import logging
-import discord
 
-from bot.settings import settings
-from bot.english import is_english_word_or_phrase
+import discord
+from discord import app_commands
+
 from bot.anki import add_to_anki
+from bot.english import is_english_word_or_phrase
+from bot.settings import settings
 
 logger = logging.getLogger(__name__)
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
 intents.message_content = True
 
-COMMAND = "!add"
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
 
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
+    await tree.sync()
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@tree.command(
+    name="add-word",
+    description="Add English word or phrase to Anki. Only uesho can be used.",
+)
+async def add_english(interaction: discord.Interaction, text: str):
+    print(str(interaction.user))
+    if str(interaction.user) != "uesholib":
+        await interaction.response.send_message(
+            f"{interaction.user} のメッセージは禁止してるよ"
+        )
         return
 
-    if not message.content.startswith(COMMAND):
-        return
-
-    if str(message.author) != "uesholib":
-        await message.channel.send(f"{message.author} のメッセージは受け付けてないよ")
-        return
-
-    text = message.content[len(COMMAND) + 1 :].strip()
     if is_english_word_or_phrase(text):
         add_to_anki(text)
-        await message.channel.send("追加したよ")
+        await interaction.response.send_message("追加したよ")
 
 
 def discord_run():
